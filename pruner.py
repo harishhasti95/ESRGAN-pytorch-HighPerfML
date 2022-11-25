6925b52ba8e0c387039042a4779c52b82b7a6bd2
harish hasti <hhasti@ncsu.edu>
2022-11-25 16:44:44 -0500

    Update Pruner file

--- pruner.py
+++ pruner.py
@@ -14,6 +14,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--gan_pth_path', default='parameters/gan.pth')
 parser.add_argument('--psnr_pth_path', default='parameters/psnr.pth')
 parser.add_argument('--interp_pth_path', default='parameters/interp.pth')
+parser.add_argument('--masks_pth_path', default='parameters/masks.pth')
 parser.add_argument('--lr_dir', default='lr_dir/')
 parser.add_argument('--sr_dir', default='sr_dir/')
 parser.add_argument('--alpha', type=int, default=0.8)
@@ -39,6 +40,20 @@ with torch.no_grad():
     net.load_state_dict(net_interp)
     net = net.to(device).eval()
     
+    for image_name in os.listdir(args.lr_dir):
+        image = Image.open(os.path.join(args.lr_dir, image_name)).convert('RGB')
+        image = TF.to_tensor(image).to(device).unsqueeze(dim=0)
+
+        image = net(image)
+
+        save_image(image,  os.path.join(args.sr_dir, image_name))
+        print(f'save {image_name}')
+    
+# Use pruner to save the new weights
+with torch.no_grad():
+    net = ESRGAN(3, 3, scale_factor=4)
+    net.load_state_dict(net_interp)
+    net = net.to(device).eval()
     
     config_list = [{
         'sparsity': 0.8,
@@ -49,7 +64,6 @@ with torch.no_grad():
     pruner.show_pruned_weights()
     pruner._unwrap_model()
     
-    
     load_dict = OrderedDict()
     for i in masks.keys():
         temp_outer = str(i)
@@ -57,10 +71,12 @@ with torch.no_grad():
             temp_inner = str(j)
             temp = temp_outer + '.' + temp_inner
             load_dict[temp] = masks[i][temp_inner]
-    torch.save(load_dict, 'parameters\masks.pth')
-    
+    torch.save(load_dict, args.masks_pth_path)
+
+# Load the pruned model to test for PSNR Ratio
+with torch.no_grad():
     net = ESRGAN(3, 3, scale_factor=4)
-    net.load_state_dict(torch.load('parameters\masks.pth'))
+    net.load_state_dict(torch.load(args.masks_pth_path))
     net = net.to(device).eval()
 
     for image_name in os.listdir(args.lr_dir):
